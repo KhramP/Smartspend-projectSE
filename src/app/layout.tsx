23ec8df83c095d8b62d669db457fa15425420318
KhramP <khramtime@gmail.com>
2026-04-01 20:38:20 +0700

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Kanit } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const kanit = Kanit({
+  subsets: ["latin", "thai"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-kanit",
 });
 
 const geistMono = Geist_Mono({
@@ -25,7 +26,12 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${kanit.variable} ${geistMono.variable} antialiased`}
+        style={{
+          fontFamily: "var(--font-kanit), system-ui, sans-serif",
+        }}
+      >
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
